4334e6ca1c54f1708658e7b9de9c009f14ce0571
Sourabh Bundele <sbundele0318@gmail.com>
2024-01-01 18:20:40 +0530

    Added main function taking input

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("Hello World")
+
+def main():
+    size = int(input("Enter the size of the maze: "))
+    walls = int(input("Enter the wall percentage you want in the maze(1-25): "))
+    
+    while True:
+        choice = input("Enter you choice (1.Print Path, 2.Generate Another Puzzle, 3.Exit): ")
+        
+        if choice == '1':
+            print("path_finder")
+        elif choice == '2':
+            print("generated another path")
+        elif choice == '3':
+            print("Thank you for using the app!")
+            break
+
+main()
