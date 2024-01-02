a39206a464a2609f5c58f841b6b0758a2450f994
Sourabh Bundele <sbundele0318@gmail.com>
2024-01-02 23:47:46 +0530

    Added Generate_Maze Feature

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
+import random
+
+def generateMaze(size, wall):
+    maze = [[" " for col in range(size)] for row in range(size)]
+    
+    for i in range(size):
+        for j in range(size):
+            if random.randint(1, 60) <= wall:
+                maze[i][j] = "#"
+    
+    maze[0][0] = 'S'
+    maze[size-1][size-1] = "E"
+    
+    return maze
+    
 
 def main():
     size = int(input("Enter the size of the maze: "))
-    walls = int(input("Enter the wall percentage you want in the maze(1-25): "))
+    wall = int(input("Enter the wall percentage you want in the maze(1-25): "))
+    
+    maze = generateMaze(size, wall)
+    print("Generated Maze")
+    for row in maze:
+        print(row)
     
     while True:
         choice = input("Enter you choice (1.Print Path, 2.Generate Another Puzzle, 3.Exit): ")
@@ -9,9 +29,16 @@ def main():
         if choice == '1':
             print("path_finder")
         elif choice == '2':
-            print("generated another path")
+            size = int(input("Enter the size of the maze: "))
+            wall = int(input("Enter the wall percentage you want in the maze(1-25): "))
+            maze = generateMaze(size, wall)
+            print("Generated Maze")
+            for row in maze:
+                print(row)
         elif choice == '3':
             print("Thank you for using the app!")
             break
+        else:
+            print("Invalid Input")
 
 main()
